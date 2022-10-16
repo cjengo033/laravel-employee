3f65f0bd51a76a92f17f316b72939103b7c47a64
cjengo033 <201810094@fit.edu.ph>
2022-10-16 22:29:16 +0800

    FE Integration not done

--- resources/views/component/view_modal.blade.php
+++ resources/views/component/view_modal.blade.php
@@ -1,40 +1,34 @@
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{ $Title }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> X </button> --}}
             </div>
             <div class="modal-body">
-                <form action="/dashboard/edit/{{ $ID }}" method="POST">
+                <form>
                     @csrf
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Full Name</label>
                         <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Enter email" name="name" value="{{ $Name }}">
+                            placeholder="Enter email" name="name" disabled>
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Enter email" name="email" value="{{ $Email }}">
+                            placeholder="Enter email" name="email"  disabled>
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
                     </div>
-                    {{-- <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div> --}}
-
-
+            
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Update</button>
             </div>
-            </form>
         </div>
     </div>
 </div>
--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -2,8 +2,14 @@
 @include('component.url_link')
 @foreach ($data_employee as $item)
     @include('component.add_modal', ['Name' => $item->name, 'Title' => 'Add Employee'])
-    @include('component.edit_modal', ['Name' => $item->name, 'Title' => 'Edit User'])
-    @include('component.delete_modal', ['ID' => $item->id, 'Title' => 'Delete User'])
+    @include('component.edit_modal', [
+        'Name' => $item->name,
+        'Email' => $item->email,
+        'ID' => $item->id,
+        'Title' => 'Edit User',
+    ])
+  
+ 
 @endforeach
 
 <div class="shadow-lg p-3 mb-5 rounded m-5">
@@ -15,39 +21,48 @@
     <table class="table text-center mt-3">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Full Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Created At</th>
-                <th scope="col">Action</th>
+                {{-- <th scope="col">#</th> --}}
+                <th scope="col">FULL NAME</th>
+                <th scope="col">EMAIL</th>
+                <th scope="col">CREATED AT</th>
+                <th scope="col">ACTION</th>
             </tr>
         </thead>
         @foreach ($data_employee as $item)
+        
             <tbody>
                 <tr>
-                    <th scope="row">{{ $item->id }}</th>
+                    {{-- @include('component.delete_modal', ['ID' => $item->id, 'Title' => 'Delete User']) --}}
+                    {{-- <th scope="row">{{ $item->id }}</th> --}}
                     <td>{{ $item->name }}</td>
                     <td>{{ $item->email }}</td>
                     <td>{{ $item->created_at }}</td>
+                    <td>{{ $item->id}}</td>
                     <td>
                         {{-- <a href="#">Delete</a>  --}}
                         {{-- <a href="/dashboard/show_user/{{ $item->id }}">Show</a> --}}
 
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                             data-bs-target="#editModal">
-                            Show User
+                            Edit User
                         </button>
-
 
                         <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                             data-bs-target="#deleteModal">
                             Delete User
                         </button>
+                        
+                        <button type="button" class="btn btn-info" data-bs-toggle="modal"
+                            data-bs-target="#viewModal">
+                            {{$item->id}}
+                        </button>
+
+                        
+                            @include('component.view_modal')
+                 
                     </td>
                 </tr>
             </tbody>
         @endforeach
     </table>
 </div>
-
-
